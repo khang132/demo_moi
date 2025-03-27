0d7d137d1edd6d7038173b5119f9d5a70bd5d118
Your Name <you@example.com>
2025-03-27 09:44:02 +0700

    Lần commit đầu tiên

---
+++ APISecurityDemo/App_Start/WebApiConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http.Headers;
+using System.Web.Http;
+
+public static class WebApiConfig
+{
+    public static void Register(HttpConfiguration config)
+    {
+        // Xóa format XML
+        config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+        // Trả về JSON theo mặc định
+        config.Formatters.JsonFormatter.SupportedMediaTypes
+            .Add(new MediaTypeHeaderValue("application/json"));
+
+        // Cấu hình route
+        config.MapHttpAttributeRoutes();
+    }
+}
---
+++ APISecurityDemo/Controllers/DemoController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace APISecurityDemo.Controllers
+{
+    public class DemoController : ApiController
+    {
+        [HttpGet]
+        [Route("api/demo")]
+        public IHttpActionResult GetMessage()
+        {
+            return Ok("API Security Demo đang chạy!");
+        }
+    }
+}
